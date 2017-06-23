a7663e82fab5730188f4a0b36dc6b34febe82297
randolrs <r.shane.randolph@gmail.com>
2017-06-23 12:27:04 -0400

    real mone

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   get 'servmanity' => 'pages#servmanity'
 
+
+  get 'new_home' => 'pages#new_home'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
